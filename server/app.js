2d1df6dad6699a8336aa28cb4c92f3173dee4c8b
sarthikbhat <bhatsarthik28@gmail.com>
2021-05-21 17:59:43 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const app = express();
 
+app.use(cors());
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
