becf51a601cf746a8f9061ee3b080dddc237cfd5
Satriick <42373405+sarthikbhat@users.noreply.github.com>
2022-04-01 19:23:33 +0530

    Update Document.js

--- client/src/components/Document.js
+++ client/src/components/Document.js
@@ -50,7 +50,7 @@ export default function Document({socket}) {
     wrapper.append(editor)
     const q = new Quill(editor, { theme: 'snow', modules: { toolbar: toolbarOptions } })
     q.disable()
-    q.setText('Please wait while we set up !!')
+//     q.setText('Please wait while we set up !!')
     q.formatLine(4, 4, 'align', 'center'); 
     setQuill(q);
   }, [])
